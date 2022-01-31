27e3dad65e5dc30eb8e3286f76afe6f4efbaa4aa
priority-healthcare <49161979+priority-healthcare@users.noreply.github.com>
2022-01-31 11:51:54 -0500

    Change text
    Changed the text.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ data }) {
         <PageMargin>
           <LogoStyles src={pageData.logoUrl} alt={pageData.title} />
           <AnnouncementStyles>Site coming soon!!</AnnouncementStyles>
-          test 2
+          test 10
         </PageMargin>
       </div>
       <Footer />
